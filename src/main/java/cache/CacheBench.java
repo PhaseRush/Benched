f22ec5d4aabfa4aaa8a02bd35327ef92795c192d
phaserush <leozhang888@yahoo.ca>
2020-03-24 16:35:08 -0700

    Added a bunch more cache benchmarks

--- src/main/java/cache/CacheBench.java
+++ src/main/java/cache/CacheBench.java
@@ -10,17 +10,28 @@ import org.openjdk.jmh.runner.options.OptionsBuilder;
 import java.util.concurrent.TimeUnit;
 
 @BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
 @State(Scope.Benchmark)
 @Fork(value = 2, jvmArgs = {"-Xms2G", "-Xmx2G"})
 @Warmup(iterations = 3)
 @Measurement(iterations = 5)
 
 /*
- * Benchmark                    (N)  Mode  Cnt     Score     Error  Units
  * Ryzen 5 3600 @ 4100Mhz, DDR4 3800MHz CL 16 1T
+ * Benchmark                    (N)  Mode  Cnt     Score    Error  Units
  * CacheBench.columnMajorSum  10000  avgt    5  1245.935 ± 37.960  ms/op
  * CacheBench.rowMajorSum     10000  avgt    5    41.639 ±  3.543  ms/op
+ * CacheBench.columnMajorSum   1000  avgt    5     1.341 ±  0.350  ms/op
+ * CacheBench.rowMajorSum      1000  avgt    5     0.256 ±  0.008  ms/op
+ *
+
+ * CacheBench.columnMajorSum  100  avgt    5  4843.406 ± 575.563  ns/op
+ * CacheBench.rowMajorSum     100  avgt    5  2468.049 ±  36.058  ns/op
+ *
+ * CacheBench.columnMajorSum   10  avgt    5  53.766 ± 1.654    ns/op
+ * CacheBench.rowMajorSum      10  avgt    5  31.054 ± 1.079    ns/op
+ *
+ *
  *
  * Ryzen 5 3600 @ 4100Mhz, DDR4 2400MHz CL 16 2T
  * CacheBench.columnMajorSum  10000  avgt    5  1195.510 ± 124.247  ms/op
@@ -28,7 +39,7 @@ import java.util.concurrent.TimeUnit;
  */
 public class CacheBench {
 
-    @Param({"10000"})
+    @Param({"10"})
     private int N;
 
     private long[][] matrix;
