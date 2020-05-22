d7f1bb014b7d8203e9eebaede4aa293cedeca53d
phaserush <leozhang888@yahoo.ca>
2020-05-21 19:26:49 -0700

    added stoi results

--- src/main/java/math/StoiBench.java
+++ src/main/java/math/StoiBench.java
@@ -18,7 +18,11 @@ import java.util.concurrent.TimeUnit;
 @Measurement(iterations = 5)
 
 /*
-
+Benchmark                 Mode  Cnt    Score   Error  Units
+StoiBench.baselineDouble  avgt    5    3.604 ± 0.004  ns/op
+StoiBench.baselineString  avgt    5    3.649 ± 0.112  ns/op
+StoiBench.doubleToString  avgt    5   79.848 ± 2.180  ns/op
+StoiBench.stringToDouble  avgt    5  147.321 ± 1.438  ns/op
  */
 public class StoiBench {
     String s;
