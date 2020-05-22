ceee885d2cff7252b52b4ecece717b098f5d0b37
phaserush <leozhang888@yahoo.ca>
2020-05-22 15:55:24 -0700

    Sum benchmark

---
+++ src/main/java/math/SumBench.java
@@ -0,0 +1,48 @@
+package math;
+
+import org.openjdk.jmh.annotations.*;
+import org.openjdk.jmh.infra.Blackhole;
+import org.openjdk.jmh.runner.Runner;
+import org.openjdk.jmh.runner.RunnerException;
+import org.openjdk.jmh.runner.options.OptionsBuilder;
+
+import java.util.concurrent.TimeUnit;
+
+@BenchmarkMode(Mode.AverageTime)
+@OutputTimeUnit(TimeUnit.NANOSECONDS)
+@State(Scope.Benchmark)
+@Fork(value = 2, jvmArgs = {"-Xms6G", "-Xmx6G"})
+@Warmup(iterations = 3)
+@Measurement(iterations = 5)
+
+/*
+Benchmark            (N)  Mode  Cnt      Score       Error  Units
+SumBench.loop  100000000  avgt    5  86000.413 ± 19418.827  us/op
+SumBench.math  100000000  avgt    5      0.004 ±     0.001  us/op
+ */
+public class SumBench {
+    @Param({"100000000"})
+    long N;
+
+    public static void main(String[] args) throws RunnerException {
+        new Runner(new OptionsBuilder()
+                .include(SumBench.class.getSimpleName())
+                .forks(1)
+                .build())
+                .run();
+    }
+
+    @Benchmark
+    public void math(Blackhole bh) {
+        bh.consume(N * (N + 1) / 2);
+    }
+
+    @Benchmark
+    public void loop(Blackhole bh) {
+        long count = 0;
+        for (int i = 0; i < N; i++) {
+            count += i;
+        }
+        bh.consume(count);
+    }
+}
