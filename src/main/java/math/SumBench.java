d51c776dad4d2aae2e04b358fb9cf5b33dedd148
phaserush <leozhang888@yahoo.ca>
2020-05-23 19:51:43 -0700

    added some bigint benchmarks

--- src/main/java/math/SumBench.java
+++ src/main/java/math/SumBench.java
@@ -6,13 +6,15 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
 
+import java.math.BigInteger;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.LongStream;
+import java.util.stream.Stream;
 
 @BenchmarkMode(Mode.AverageTime)
-@OutputTimeUnit(TimeUnit.MICROSECONDS)
+@OutputTimeUnit(TimeUnit.SECONDS)
 @State(Scope.Benchmark)
-@Fork(value = 2, jvmArgs = {"-Xms6G", "-Xmx6G"})
+@Fork(value = 2, jvmArgs = {"-Xms16G", "-Xmx16G"})
 @Warmup(iterations = 3)
 @Measurement(iterations = 5)
 
@@ -24,24 +26,33 @@ SumBench.parallelStreamReduce  100000000  avgt    5  67860.255 ± 2188.650  us/o
 SumBench.streamReduce          100000000  avgt    5  44787.744 ± 2993.240  us/op
  */
 public class SumBench {
-    @Param({"100000000"})
+    @Param({"1000000000"})
     long N;
+
+    @Param("1000000000")
+    String M;
+
+    BigInteger m_val;
 
     public static void main(String[] args) throws RunnerException {
         new Runner(new OptionsBuilder()
                 .include(SumBench.class.getSimpleName())
-                .forks(1)
                 .build())
                 .run();
     }
 
+    @Setup()
+    public void setup() {
+        m_val = new BigInteger(M);
+    }
+
     @Benchmark
-    public void math(Blackhole bh) {
+    public void math_primitive(Blackhole bh) {
         bh.consume(N * (N + 1) / 2);
     }
 
     @Benchmark
-    public void loop(Blackhole bh) {
+    public void loop_primitive(Blackhole bh) {
         long count = 0;
         for (int i = 0; i < N; i++) {
             count += i;
@@ -50,15 +61,48 @@ public class SumBench {
     }
 
     @Benchmark
-    public void streamReduce(Blackhole bh) {
+    public void streamReduce_primitive(Blackhole bh) {
         bh.consume(LongStream.range(0, N)
                 .reduce(0, Long::sum));
     }
 
     @Benchmark
-    public void parallelStreamReduce(Blackhole bh) {
+    public void parallelStreamReduce_primitive(Blackhole bh) {
         bh.consume(LongStream.range(0, N)
                 .parallel()
                 .reduce(0, Long::sum));
+    }
+
+    @Benchmark
+    public void math_bigint(Blackhole bh) {
+        bh.consume(m_val.multiply(m_val.add(BigInteger.ONE)).divide(BigInteger.TWO));
+    }
+
+    @Benchmark
+    public void loop_bigint(Blackhole bh) {
+        BigInteger count = BigInteger.ZERO;
+        for (BigInteger i = BigInteger.ZERO; i.compareTo(m_val) != 0; i = i.add(BigInteger.ONE)) {
+            count = count.add(i);
+        }
+        bh.consume(count);
+    }
+
+    @Benchmark
+    public void streamReduce_bigint(Blackhole bh) {
+        BigInteger indexer = BigInteger.ZERO;
+        bh.consume(
+                Stream.generate(() -> indexer.add(BigInteger.ONE))
+                        .takeWhile(i -> i.compareTo(m_val) < 0)
+                        .reduce(BigInteger::add)
+        );
+    }
+
+    @Benchmark
+    public void parallelStreamReduce_bigint(Blackhole bh) {
+        BigInteger indexer = BigInteger.ZERO;
+        bh.consume(Stream.generate(() -> indexer.add(BigInteger.ONE))
+                .parallel()
+                .takeWhile(i -> i.compareTo(m_val) < 0)
+                .reduce(BigInteger::add));
     }
 }
